b83dd5c712bae3dbe9000ec1a24327f5505eedcf
KASolomon <kusiappiahsolomon10@gmail.com>
2024-08-24 01:23:37 +0000

    Create app context variable

--- src/config/AppContext.tsx
+++ src/config/AppContext.tsx
@@ -1,20 +1,39 @@
-import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
 
+interface AppData {
+  totalPlayers: number;
+  playersPerTeam: number;
+  teams : any []
+}
 interface ContextShape {
-  appData: {};
-  setAppData: Dispatch<SetStateAction<{}>>;
+  appData: AppData;
+  setAppData: Dispatch<SetStateAction<AppData>>;
 }
 
-export const StartingXIContext = createContext<ContextShape>({appData :{}, setAppData : ()=> null});
+export const StartingXIContext = createContext<ContextShape>({
+  appData: { totalPlayers: 0, playersPerTeam: 0, teams: [] },
+  setAppData: () => null,
+});
 
 interface Props {
   children: ReactNode;
 }
 const AppContext = ({ children }: Props) => {
-  const [appData, setAppData] = useState({});
+  const [appData, setAppData] = useState<AppData>({
+    totalPlayers: 0,
+    playersPerTeam: 0,
+    teams: [],
+  });
 
   return (
-    <StartingXIContext.Provider value={{ appData, setAppData }}>
+    <StartingXIContext.Provider value={{ appData, setAppData
+    }}>
       {children}
     </StartingXIContext.Provider>
   );
