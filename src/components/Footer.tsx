449a5c8aa0a0a3863940378dbc55219cc3a92a5a
KASolomon <kusiappiahsolomon10@gmail.com>
2024-08-24 03:02:15 +0000

    improve footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Text style={{ fontSize: "x-small", textAlign: "center" }}>
+      <a href="http://www.freepik.com" target="_blank">
+        Page icon designed by freepik
+      </a>
+    </Text>
+  );
+};
+
+export default Footer;
